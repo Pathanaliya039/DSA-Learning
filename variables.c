b37611b762860837204af536853335c8d4ae945c
Aliya Pathan <pathanaliya563@gmail.com>
2026-08-12 20:26:36 +0530

    Day 1: C Programming Basic

---
+++ variables.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int age = 20;
+    float marks = 85.5;
+    char grade = 'A';
+
+    printf("Age: %d\n", age);
+    printf("Marks: %.1f\n", marks);
+    printf("Grade: %c\n",grade);
+
+    return 0;
+}
