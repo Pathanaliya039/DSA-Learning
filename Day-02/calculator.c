db22ac7fd218ec2c3d6c9fe06c129e9056ca6400
Aliya Pathan <pathanaliya563@gmail.com>
2026-08-13 12:43:41 +0530

    Day 1 and Day 2:CBasics and Input

---
+++ Day-02/calculator.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, sum;
+
+    printf("Enter first number:");
+    scanf("%d", &a);
+
+    printf("Enter second number:");
+    scanf("%d", &b);
+
+    int division = a / b;
+
+    printf("div = %d\n", b);
+
+    return 0;
+}
---
+++ Day-02/two_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+
+    printf("Enter first number:");
+    scanf("%d", &a);
+
+    printf("Enter second number:");
+    scanf("%d", &b);
+
+    printf("First number: %d\n", a);
+    printf("Second number:%d\n", b);
+
+    return 0;
+}
